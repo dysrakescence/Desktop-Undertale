2a11c47ed24638c25edda7de89f3118dee1d87ac
dma-2048 <54641605+dma-2048@users.noreply.github.com>
2021-06-14 14:59:41 -0400

    Add files via upload

---
+++ button_functions.py
@@ -0,0 +1,14 @@
+def toggle_sidebar(sidebar):
+    sidebar.hidden = not sidebar.hidden
+
+def button_function_2():
+    pass
+
+def button_function_3():
+    pass
+
+def button_function_4():
+    pass
+
+def button_function_5():
+    pass
