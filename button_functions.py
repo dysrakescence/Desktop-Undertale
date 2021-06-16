0f52077097c500da453067f7cd9f1ceb842b22ef
ChristopherSun <54641605+dma-2048@users.noreply.github.com>
2021-06-15 21:23:18 -0400

    Sidebar changes
    Updated the sidebar interface to the Undertale format. 2 out of 4 green buttons are now working.

--- button_functions.py
+++ button_functions.py
@@ -1,14 +1,67 @@
-def toggle_sidebar(sidebar):
-    sidebar.hidden = not sidebar.hidden
+import pygame
+from pygame.freetype import Font, init
+from json import load
 
-def button_function_2():
-    pass
+init()
+comic_sans = Font("fonts/comic_sans.ttf", 15)
+papyrus = Font("fonts/papyrus.ttf", 15)
+determination_mono = Font("fonts/determination_mono.otf", 40/3)
+determination_sans = Font("fonts/determination_sans.otf", 40/3)
+file = open("stats.json")
+stats = load(file)
+file.close()
+wellness = stats["wellness"]
+items = stats["items"]
 
-def button_function_3():
-    pass
+def toggle_wellness_sidebar(sidebar):
+	if sidebar.hidden:
+		sidebar.hidden = False
+		for i, attribute in enumerate(wellness.keys()):
+			sidebar.add(Text(sidebar.window, sidebar.rect.x + 5, sidebar.rect.y + 5 + (i * 35), determination_sans, f"{attribute}:", (255, 255, 255), (0, 0, 0)))
+			sidebar.add(Text(sidebar.window, sidebar.rect.x + 5, sidebar.rect.y + 23 + (i * 35), determination_sans, str(wellness[attribute]), (255, 255, 255), (0, 0, 0)))
+			sidebar.add(ProgressBar(sidebar.window, sidebar.rect.x + 30, sidebar.rect.y + 22 + (i * 35), 80, 12, (0, 0, 255)))
+	else:
+		sidebar.hidden = True
+		sidebar.clear()
 
-def button_function_4():
-    pass
+def toggle_items_sidebar(sidebar):
+	if sidebar.hidden:
+		sidebar.hidden = False
+		for i, item in enumerate(items):
+			sidebar.add(Text(sidebar.window, sidebar.rect.x + 5, sidebar.rect.y + 5 + (i * 20), determination_sans, f"- {item}", (255, 255, 255), (0, 0, 0)))
+	else:
+		sidebar.hidden = True
+		sidebar.clear()
 
-def button_function_5():
-    pass
+class Text:
+	def __init__(self, window: pygame.Surface, x: int, y: int, font: Font, text: str, color: tuple, background: tuple = None):
+		self.window = window
+		self.rect = pygame.Rect(x, y, 0, 0)
+		self.font = font
+		self.color = color
+		self.background = background
+		self.text = font.render(text, color, background)[0]
+
+	def move(self, x: int, y: int):
+		self.rect.x, self.rect.y = x, y
+
+	def draw(self):
+		self.window.blit(self.text, (self.rect.x, self.rect.y))
+
+	def edit(self, text: str):
+		self.text = self.font.render(text, self.color, self.background)[0]
+
+class ProgressBar:
+	def __init__(self, window: pygame.Surface, x: int, y: int, width: int, height: int, color: tuple):
+		self.window = window
+		self.border = pygame.Rect(x - 1, y - 1, width + 2, height + 2)
+		self.rect = pygame.Rect(x, y, width, height)
+		self.color = color
+
+	def move(self, x: int, y: int):
+		self.border.x, self.border.y = x - 1, y - 1
+		self.rect.x, self.rect.y = x, y
+
+	def draw(self):
+		pygame.draw.rect(self.window, (255, 255, 255), self.border)
+		pygame.draw.rect(self.window, self.color, self.rect)
--- sans.py
+++ sans.py
@@ -1,9 +1,11 @@
 import pygame
+import pygame.freetype
 import sys
 import win32api
 import win32con
 import win32gui
-
+from button_functions import toggle_wellness_sidebar, toggle_items_sidebar
+#reminder to turn Program() into init() like how pygame does it
 class Program:
 	def __init__(self):
 		pygame.init()
@@ -16,7 +18,7 @@ class Program:
 		win32gui.SetLayeredWindowAttributes(systemID, win32api.RGB(*self.transparentColor), 0, win32con.LWA_COLORKEY)
 		win32gui.SetWindowPos(systemID, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
 		self.clock = pygame.time.Clock()
-		self.pet = self.Pet(self.screen, 250, 250, 80, 80)
+		self.pet = self.Pet(self.screen, 250, 250, 100, 100)
 
 	def run(self):
 		while True:
@@ -50,32 +52,35 @@ class Program:
 	class Pet:
 		def __init__(self, window: pygame.Surface, x: int, y: int, width: int, height: int):
 			self.window = window
-			self.x, self.y = x, y
 			self.rect = pygame.Rect(x, y, width, height)
 			self.draging = False
-			self.toolbar = self.Toolbar(window, x - 50, y + 100, 180, 40)
+			self.sidebar = self.Sidebar(window, x - 140, y - 80, 120, 180)
+			self.toolbar = self.Toolbar(window, x - 40, y + 120, 180, 40, (0, 128, 128), self.sidebar)
 
 		def move(self, x: int, y: int):
 			self.rect.x, self.rect.y = x, y
-			self.toolbar.move(x - 50, y + 100)
+			self.toolbar.move(x - 40, y + 120)
+			self.sidebar.move(x - 140, y - 80)
 
 		def draw(self):
 			pygame.draw.rect(self.window, (0, 150, 255), self.rect)
+			self.sidebar.draw() if not self.sidebar.isHidden() else None
 			self.toolbar.draw() if not self.toolbar.isHidden() else None
 
 		def isClicked(self, mousePos: tuple):
 			return self.rect.collidepoint(mousePos)
 
 		class Toolbar:
-			def __init__(self, window: pygame.Surface, x: int, y: int, width: int, height: int):
+			def __init__(self, window: pygame.Surface, x: int, y: int, width: int, height: int, color: tuple, sidebar):
 				self.window = window
-				self.x, self.y = x, y
 				self.rect = pygame.Rect(x, y, width, height)
-				button1 = self.Button(window, x + 5, y + 5, 30, 30, (95, 146, 40), lambda: print(1))
-				button2 = self.Button(window, x + 40, y + 5, 30, 30, (95, 146, 40), lambda: print(2))
-				button3 = self.Button(window, x + 75, y + 5, 30, 30, (95, 146, 40), lambda: print(3))
-				button4 = self.Button(window, x + 110, y + 5, 30, 30, (95, 146, 40), lambda: print(4))
-				button5 = self.Button(window, x + 145, y + 5, 30, 30, (241, 60, 32), Program.end)
+				self.color = color
+				self.sidebar = sidebar
+				button1 = Program.Pet.Button(window, x + 5, y + 5, 30, 30, (95, 146, 40), lambda: toggle_wellness_sidebar(self.sidebar))
+				button2 = Program.Pet.Button(window, x + 40, y + 5, 30, 30, (95, 146, 40), lambda: toggle_items_sidebar(self.sidebar))
+				button3 = Program.Pet.Button(window, x + 75, y + 5, 30, 30, (95, 146, 40), lambda: toggle_wellness_sidebar(self.sidebar))
+				button4 = Program.Pet.Button(window, x + 110, y + 5, 30, 30, (95, 146, 40), lambda: toggle_items_sidebar(self.sidebar))
+				button5 = Program.Pet.Button(window, x + 145, y + 5, 30, 30, (241, 60, 32), Program.end)
 				self.buttons = (button1, button2, button3, button4, button5)
 
 			def move(self, x: int, y: int):
@@ -84,7 +89,7 @@ class Program:
 				self.rect.x, self.rect.y = x, y
 
 			def draw(self):
-				pygame.draw.rect(self.window, (0, 128, 128), self.rect)
+				pygame.draw.rect(self.window, self.color, self.rect)
 				mousePos = pygame.mouse.get_pos()
 				for button in self.buttons:
 					button.setColor(mousePos)
@@ -93,37 +98,65 @@ class Program:
 			def isHidden(self):
 				return not pygame.key.get_focused()
 
-			class Button:
-				def __init__(self, window: pygame.Surface, x: int, y: int, width: int, height: int, color: tuple, function):
-					self.window = window
-					self.x, self.y = x, y
-					self.width, self.height = width, height
-					self.rect = pygame.Rect(x, y, width, height)
-					self.originalColor = color
-					self.highlightedColor = (min(color[0] + 50, 255), min(color[1] + 50, 255), min(color[2] + 50, 255))
-					self.currentColor = color
-					self.function = function
+		class Button:
+			def __init__(self, window: pygame.Surface, x: int, y: int, width: int, height: int, color: tuple, function):
+				self.window = window
+				self.rect = pygame.Rect(x, y, width, height)
+				self.originalColor = color
+				self.highlightedColor = (min(color[0] + 50, 255), min(color[1] + 50, 255), min(color[2] + 50, 255))
+				self.currentColor = color
+				self.function = function
 
-				def move(self, x: int, y: int):
-					self.rect.x, self.rect.y = x, y
+			def move(self, x: int, y: int):
+				self.rect.x, self.rect.y = x, y
 
-				def draw(self):
-					pygame.draw.rect(self.window, self.color, self.rect)
+			def draw(self):
+				pygame.draw.rect(self.window, self.color, self.rect)
 
-				def setColor(self, mousePos: tuple):
-					if self.rect.collidepoint(mousePos):
-						self.color = self.highlightedColor
-					else:
-						self.color = self.originalColor
+			def setColor(self, mousePos: tuple):
+				if self.rect.collidepoint(mousePos):
+					self.color = self.highlightedColor
+				else:
+					self.color = self.originalColor
 
-				def setStatus(self, mousePos: tuple):
-					self.callFunction() if self.rect.collidepoint(mousePos) else None
+			def setStatus(self, mousePos: tuple):
+				self.callFunction() if self.rect.collidepoint(mousePos) else None
 
-				def callFunction(self):
-					self.function()
+			def callFunction(self):
+				self.function()
+
+		class Sidebar:
+			def __init__(self, window: pygame.Surface, x: int, y: int, width: int, height: int):
+				self.window = window
+				self.border = pygame.Rect(x - 3, y - 3, width + 6, height + 6)
+				self.rect = pygame.Rect(x, y, width, height)
+				self.objects = []
+				self.hidden = True
+
+			def add(self, object):
+				self.objects.append(object)
+
+			def clear(self):
+				self.objects.clear()
+
+			def move(self, x: int, y: int):
+				for object in self.objects:
+					object.move(object.rect.x + x - self.rect.x, object.rect.y + y - self.rect.y)
+				self.border.x, self.border.y = x - 3, y - 3
+				self.rect.x, self.rect.y = x, y
+
+			def draw(self):
+				pygame.draw.rect(self.window, (255, 255, 255), self.border)
+				pygame.draw.rect(self.window, (0, 0, 0), self.rect)
+				for object in self.objects:
+					object.draw()
+
+			def isHidden(self):
+				return self.hidden
 
 def main():
 	program = Program()
 	program.run()
 
-main()
+if __name__ == "__main__":
+	main()
